1f4ca1891ac59045b9b2a11c09726f077a403784
abdallahelmadi <abdallahelmadi42@gmail.com>
2025-03-08 19:10:12 +0000

    to tommorow

--- functions/check_command_line.c
+++ functions/check_command_line.c
@@ -6,7 +6,7 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 19:32:31 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/03/07 19:44:50 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:53:00 by abdael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	check_command_line(const char *command_line)
 {
+	printf("> (%s)\n", command_line);
 	if (utils_strcmp(command_line, ' ') == 0)
 		return (1);
 	return (0);
--- functions/signals/signals_sigint.c
+++ functions/signals/signals_sigint.c
@@ -6,7 +6,7 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:33:31 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/03/08 17:34:00 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/03/08 19:09:49 by abdael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,4 @@
 void	signals_sigint(int sig)
 {
 	(void)sig;
-	// rl_on_new_line();
-	// rl_replace_line("test", 1);
-	// rl_redisplay();
 }
