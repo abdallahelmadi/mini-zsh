ba31dc590e012bef5f4c9f582f0199612c807420
Batrii <dhj.nafiai@gmail.com>
2025-03-14 15:14:24 +0000

    norm

--- functions/check_command_line.c
+++ functions/check_command_line.c
@@ -6,7 +6,7 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 19:32:31 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/03/14 15:05:38 by bnafiai          ###   ########.fr       */
+/*   Updated: 2025/03/14 15:14:05 by bnafiai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,5 @@ void	check_command_line(const char *command_line)
 		g_lastexitstatus = FAILURE;
 	}
 	else
-	{
 		g_lastexitstatus = SUCCESS;
-	}
 }
