5b6860494b945afe76607d40afa687c2f1e11d0f
abdallahelmadi <abdallahelmadi42@gmail.com>
2025-04-02 10:57:10 +0000

    the parsin has been successfuly

--- functions/parsin/parsin_global.c
+++ functions/parsin/parsin_global.c
@@ -6,11 +6,25 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 19:32:31 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/04/01 09:20:42 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/04/02 10:56:11 by abdael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static int	is_empty_string(const char *command_line)
+{
+	int	index;
+
+	index = 0;
+	while (command_line[index] != '\0')
+	{
+		if (command_line[index] != ' ')
+			return (SUCCESS);
+		index++;
+	}
+	return (free((char *)command_line), FAILURE);
+}
 
 void	parsin_global(const char *command_line)
 {
@@ -20,7 +34,8 @@ void	parsin_global(const char *command_line)
 	g_lastexitstatus = SUCCESS;
 	if (command_line[0] == '\0')
 		return (free((char *)command_line));
-	if (parsin_forbidden_keywords(command_line)
+	if (is_empty_string(command_line)
+		|| parsin_forbidden_keywords(command_line)
 		|| parsin_analyse_quotes(command_line))
 		return ;
 	parsin_make_list(command_line, &cmd_list);
--- functions/parsin/parsin_tokenization.c
+++ functions/parsin/parsin_tokenization.c
@@ -6,7 +6,7 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 09:19:45 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/04/01 18:11:36 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/04/02 10:53:56 by abdael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,60 @@ static void	add_token(t_cmd_line **node)
 	}
 }
 
+static char	*special_strdup(char *string)
+{
+	char	*newstring;
+	int		index;
+	int		indexofnewsreing;
+
+	newstring = (char *)malloc(sizeof(char) * utils_strlen(string) + 1);
+	if (newstring == NULL)
+		return (NULL);
+	index = 0;
+	indexofnewsreing = 0;
+	while (string[index] != '\0')
+	{
+		if (string[index] == 1)
+			index++;
+		else
+		{
+			newstring[indexofnewsreing] = string[index];
+			index++;
+			indexofnewsreing++;
+		}
+	}
+	newstring[indexofnewsreing] = '\0';
+	free(string);
+	return (newstring);
+}
+
 static void	remove_qoutes(t_cmd_line **node)
 {
-	(void)node;
-	return ;
+	int		index;
+	int		inside_double;
+	int		inside_single;
+	char	*newstring;
+
+	index = -1;
+	inside_double = 0;
+	inside_single = 0;
+	while (((*node)->data)[++index] != '\0')
+	{
+		if (((*node)->data)[index] == '\"' && inside_single != 1
+			&& inside_single % 2 == 0)
+		{
+			((*node)->data)[index] = 1;
+			inside_double++;
+		}
+		else if (((*node)->data)[index] == '\'' && inside_double != 1
+			&& inside_double % 2 == 0)
+		{
+			((*node)->data)[index] = 1;
+			inside_single++;
+		}
+	}
+	newstring = special_strdup((*node)->data);
+	(*node)->data = newstring;
 }
 
 void	parsin_tokenization(t_cmd_line **cmd_list)
