a8a6c8c932344884fd545445ccff9aa668076abe
Batrii <dhj.nafiai@gmail.com>
2025-04-08 17:57:33 +0100

    make some changes in file execution_global and add function is_builtin

--- functions/execution/execution_global.c
+++ functions/execution/execution_global.c
@@ -6,7 +6,7 @@
 /*   By: bnafiai <bnafiai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 08:39:04 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/04/06 19:26:02 by bnafiai          ###   ########.fr       */
+/*   Updated: 2025/04/08 17:55:54 by bnafiai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,21 +53,21 @@ void	execution_v(t_cmd_line *node)
 		tmp = tmp->next;
 	}
 	args[j] = NULL;
-	path = getenv("PATH");
+	path = utils_getenv("PATH");
 	dirs = utils_split(path, ':');
 	while (dirs[i])
 	{
 		full_path = utils_strjoin(dirs[i], "/", node->data);
 		if (access(full_path, X_OK) == 0)
 		{
-			break;
+			execve(full_path, args, g_global.g_environments);
+			free(full_path);
+			free(path);
+			utils_free(args);
 		}
 		i++;
 	}
-	if (!dirs[i])
-		perror("Command not exist");
-	else
-		execv(full_path, args);
+	utils_free(dirs);
 }
 void	execution_part(t_cmd_line **node)
 {
@@ -86,20 +86,13 @@ void	execution_part(t_cmd_line **node)
 	else
 	{
 		close(fd[1]);
-		char	buffer[1024];
-		int		readed;
-		readed = read(fd[0], buffer, sizeof(buffer) - 1);
-		buffer[readed] = '\0';
-		printf("child execution : %s", buffer);
 		close(fd[0]);
 		waitpid(pid, &status, 0);
 	}
 }
-void	execution_global(t_cmd_line **cmd_list)
+static void	is_builtin(t_cmd_line *node)
 {
-	if (execution_syntax(cmd_list))
-		return ;
-	t_cmd_line *x = *cmd_list;
+	t_cmd_line *x = node;
 	if (x->type == 0 && utils_strstr_pro(x->data, "export"))
 		builtin_export(x);
 	else if (x->type == 0 && utils_strstr_pro(x->data, "unset"))
@@ -114,7 +107,28 @@ void	execution_global(t_cmd_line **cmd_list)
 		builtin_echo(x);
 	else if (x->type == 0 && utils_strstr_pro(x->data, "cd"))
 		builtin_cd(x);
-	else
+}
+void	execution_global(t_cmd_line **cmd_list)
+{
+	if (execution_syntax(cmd_list))
+		return ;
+	// t_cmd_line *x = *cmd_list;
+	// if (x->type == 0 && utils_strstr_pro(x->data, "export"))
+	// 	builtin_export(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "unset"))
+	// 	builtin_unset(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "env"))
+	// 	builtin_env(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "pwd"))
+	// 	builtin_pwd(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "exit"))
+	// 	builtin_exit(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "echo"))
+	// 	builtin_echo(x);
+	// else if (x->type == 0 && utils_strstr_pro(x->data, "cd"))
+	// 	builtin_cd(x);
+	// else
 		execution_part(cmd_list);
+	// is_builtin(*cmd_list);
 	utils_free_list(cmd_list);
 }
