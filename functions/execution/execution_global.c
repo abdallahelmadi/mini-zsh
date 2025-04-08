39f73e908fe743d15716f83dace6ca554ae0b66f
Batrii <dhj.nafiai@gmail.com>
2025-04-08 17:45:39 +0100

    test try for execution part

--- functions/execution/execution_global.c
+++ functions/execution/execution_global.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_global.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bnafiai <bnafiai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 08:39:04 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/04/06 12:04:30 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:45:06 by bnafiai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,70 @@ void	execution_global(t_cmd_line **cmd_list)
 {
 	if (execution_syntax(cmd_list))
 		return ;
+
+	// t_cmd_line *curr = *cmd_list;
+	// int		prev_read = 0;
+	// int		fds[2];
+	// pid_t	pid;
+
+	// while (curr)
+	// {
+	// 	pipe(fds);
+	// 	pid = fork();
+	// 	if (pid == 0)
+	// 	{
+	// 		// Input from previous pipe
+	// 		if (prev_read != 0)
+	// 		{
+	// 			dup2(prev_read, 0);
+	// 			close(prev_read);
+	// 		}
+	// 		// Output to current pipe (only if another command follows)
+	// 		if (curr->next && curr->next->type == TP_PIPE)
+	// 		{
+	// 			dup2(fds[1], 1);
+	// 		}
+	// 		close(fds[0]);
+	// 		close(fds[1]);
+
+	// 		// Collect args
+	// 		t_cmd_line *tmp = curr;
+	// 		int count = 0;
+	// 		while (tmp && tmp->type != TP_PIPE)
+	// 		{
+	// 			count++;
+	// 			tmp = tmp->next;
+	// 		}
+
+	// 		char **args = malloc(sizeof(char *) * (count + 1));
+	// 		tmp = curr;
+	// 		for (int i = 0; i < count; i++)
+	// 		{
+	// 			args[i] = tmp->data;
+	// 			tmp = tmp->next;
+	// 		}
+	// 		args[count] = NULL;
+
+	// 		char *full_path = utils_strjoin("/usr/bin/", curr->data, "");
+	// 		execve(full_path, args, NULL);
+	// 		perror("execve");
+	// 		free(full_path);
+	// 		free(args);
+	// 		exit(1);
+	// 	}
+	// 	else
+	// 	{
+	// 		wait(NULL);
+	// 		close(fds[1]);
+	// 		if (prev_read != 0)
+	// 			close(prev_read);
+	// 		prev_read = fds[0];
+	// 	}
+	// 	// Skip to next command after pipe
+	// 	while (curr && curr->type != TP_PIPE)
+	// 		curr = curr->next;
+	// 	if (curr)
+	// 		curr = curr->next;
+	// }
 	utils_free_list(cmd_list);
 }
-
-// ---------------------------------------------------------------------
-// t_cmd_line *x = *cmd_list;
-// if (x->type == 0 && utils_strstr_pro(x->data, "export"))
-// 	builtin_export(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "unset"))
-// 	builtin_unset(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "env"))
-// 	builtin_env(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "pwd"))
-// 	builtin_pwd(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "exit"))
-// 	builtin_exit(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "echo"))
-// 	builtin_echo(x);
-// else if (x->type == 0 && utils_strstr_pro(x->data, "cd"))
-// 	builtin_cd(x);
-// ---------------------------------------------------------------------
